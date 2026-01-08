55b93295173cd22e1cbcd7c5222c7e84567ab0b2
Nishant Sharma <197467920+Nishant-444@users.noreply.github.com>
2026-01-08 07:56:37 +0530

    refactor: update Prisma client initialization and enhance CloudinaryResponse interface

--- src/types/cloudinary.types.ts
+++ src/types/cloudinary.types.ts
@@ -1,5 +1,10 @@
 export interface CloudinaryResponse {
   url: string;
   public_id: string;
-  secure_url?: string; // https version
+  secure_url?: string;
+  duration?: number;
+  format?: string; // mp4, jpg
+  width?: number;
+  height?: number;
+  resource_type?: string; // img, vid
 }
