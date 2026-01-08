173333e6dd47a8e7d0482aa3d839197fe510e827
Nishant Sharma <197467920+Nishant-444@users.noreply.github.com>
2026-01-08 08:46:54 +0530

    refactor: update JWT verification to use Prisma for user retrieval and adjust payload structure

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,43 +1,56 @@
 import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
 import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
+import prisma from '../lib/prisma.js';
+
+// id is number not a string in postgres
+interface JWTPayload {
+  id: number;
+  email: string;
+  username: string;
+  fullname: string;
+}
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-  interface JWTPayload {
-    _id: string;
-    email: string;
-    username: string;
-    fullname: string;
-  }
-
   const token =
-    req.cookies.accessToken ||
+    req.cookies?.accessToken ||
     req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) {
-    throw new ApiError(401, 'Unauthorized');
+    throw new ApiError(401, 'Unauthorized request');
   }
 
   try {
     const decodedToken = jwt.verify(
       token,
-      process.env.ACCESS_TOKEN_SECRET
+      process.env.ACCESS_TOKEN_SECRET as string
     ) as JWTPayload;
-    const user = await User.findById(decodedToken?._id).select(
-      '-password -refreshToken'
-    );
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decodedToken.id,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        fullname: true,
+        avatar: true,
+        coverImage: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
 
     if (!user) {
-      throw new ApiError(401, 'Unauthorized');
+      throw new ApiError(401, 'Invalid Access Token');
     }
 
     req.user = user;
+
     next();
   } catch (error) {
-    if (error instanceof Error) {
-      console.error(`JWT verification failed: ${error.message}`);
-    }
-    throw new ApiError(401, 'Unauthorized. Invalid or expired token.');
+    const errorMessage = (error as any)?.message || 'Invalid access token';
+    throw new ApiError(401, errorMessage);
   }
 });
