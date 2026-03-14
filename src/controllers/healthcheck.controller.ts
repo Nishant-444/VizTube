3ed62f243a9081255a518edf57071d5f4c5a2dee
Nishant Sharma <197467920+Nishant-444@users.noreply.github.com>
2026-03-14 14:48:08 +0530

    fix: refactor healthcheck response to use ApiResponse class for consistency

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,11 +1,8 @@
-// import { ApiResponse } from '../utils/ApiResponse.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const healthcheck = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    status: 'success',
-    message: 'Health check passed!',
-  });
+  return res.status(200).json(new ApiResponse(200, 'Health check passed!'));
 });
 
 export { healthcheck };
